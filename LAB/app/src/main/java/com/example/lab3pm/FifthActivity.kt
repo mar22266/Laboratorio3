fd2d674b41a12cd12ffada12dc1e35e7fc94357f
Andre <andremt2003@gmail.co>
2023-08-31 19:05:38 -0600

    DIA DEL PADRE FINALIZADO

--- LAB/app/src/main/java/com/example/lab3pm/FifthActivity.kt
+++ LAB/app/src/main/java/com/example/lab3pm/FifthActivity.kt
@@ -1,6 +1,8 @@
 package com.example.lab3pm
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class FifthActivity : AppCompatActivity() {
@@ -9,6 +11,25 @@ class FifthActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fifth)
 
-        // Inicializar los botones y configurar sus onClickListeners...
+        val btnSubMenu41: Button = findViewById(R.id.btnSubMenu41)
+        btnSubMenu41.setOnClickListener {
+            val intent = Intent(this@FifthActivity, IMG13ACT::class.java)
+            startActivity(intent)
+        }
+        val btnSubMenu42: Button = findViewById(R.id.btnSubMenu42)
+        btnSubMenu42.setOnClickListener {
+            val intent = Intent(this@FifthActivity, IMG14ACT::class.java)
+            startActivity(intent)
+        }
+        val btnSubMenu43: Button = findViewById(R.id.btnSubMenu43)
+        btnSubMenu43.setOnClickListener {
+            val intent = Intent(this@FifthActivity, IMG15ACT::class.java)
+            startActivity(intent)
+        }
+        val btnSubMenu44: Button = findViewById(R.id.btnSubMenu44)
+        btnSubMenu44.setOnClickListener {
+            val intent = Intent(this@FifthActivity, IMG16ACT::class.java)
+            startActivity(intent)
+        }
     }
 }
--- LAB/app/src/main/java/com/example/lab3pm/MainActivity.kt
+++ LAB/app/src/main/java/com/example/lab3pm/MainActivity.kt
@@ -18,25 +18,25 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        val btnOption2: Button = findViewById(R.id.btnOption2) // Corregido
+        val btnOption2: Button = findViewById(R.id.btnOption2)
         btnOption2.setOnClickListener {
             val intent = Intent(this@MainActivity, ThirdActivity::class.java)
             startActivity(intent)
         }
 
-        val btnOption3: Button = findViewById(R.id.btnOption3) // Corregido
+        val btnOption3: Button = findViewById(R.id.btnOption3)
         btnOption3.setOnClickListener {
             val intent = Intent(this@MainActivity, FourthActivity::class.java)
             startActivity(intent)
         }
 
-        val btnOption4: Button = findViewById(R.id.btnOption4) // Agregado
+        val btnOption4: Button = findViewById(R.id.btnOption4)
         btnOption4.setOnClickListener {
             val intent = Intent(this@MainActivity, FifthActivity::class.java)
             startActivity(intent)
         }
 
-        val btnOption5: Button = findViewById(R.id.btnOption5) // Agregado
+        val btnOption5: Button = findViewById(R.id.btnOption5)
         btnOption5.setOnClickListener {
             val intent = Intent(this@MainActivity, SixthActivity::class.java)
             startActivity(intent)
