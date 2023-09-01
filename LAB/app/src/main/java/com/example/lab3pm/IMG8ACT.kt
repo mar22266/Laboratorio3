dde90ce7f81025c8ecdf4c432db539782aee3788
Andre <andremt2003@gmail.co>
2023-08-31 18:38:52 -0600

    BODAS CUMPLE LISTO

---
+++ LAB/app/src/main/java/com/example/lab3pm/IMG8ACT.kt
@@ -0,0 +1,16 @@
+package com.example.lab3pm
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class IMG8ACT : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_img8)
+
+
+    }
+}
--- LAB/app/src/main/java/com/example/lab3pm/ThirdActivity.kt
+++ LAB/app/src/main/java/com/example/lab3pm/ThirdActivity.kt
@@ -1,6 +1,8 @@
 package com.example.lab3pm
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class ThirdActivity : AppCompatActivity() {
@@ -9,6 +11,26 @@ class ThirdActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third)
 
-        // Inicializar los botones y configurar sus onClickListeners...
+        val btnSubMenu21: Button = findViewById(R.id.btnSubMenu21)
+        btnSubMenu21.setOnClickListener {
+            val intent = Intent(this@ThirdActivity, IMG5ACT::class.java)
+            startActivity(intent)
+        }
+        val btnSubMenu22: Button = findViewById(R.id.btnSubMenu22)
+        btnSubMenu22.setOnClickListener {
+            val intent = Intent(this@ThirdActivity, IMG6ACT::class.java)
+            startActivity(intent)
+        }
+        val btnSubMenu23: Button = findViewById(R.id.btnSubMenu23)
+        btnSubMenu23.setOnClickListener {
+            val intent = Intent(this@ThirdActivity, IMG7ACT::class.java)
+            startActivity(intent)
+        }
+        val btnSubMenu24: Button = findViewById(R.id.btnSubMenu24)
+        btnSubMenu24.setOnClickListener {
+            val intent = Intent(this@ThirdActivity, IMG8ACT::class.java)
+            startActivity(intent)
+        }
     }
+
 }
