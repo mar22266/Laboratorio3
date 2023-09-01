d7f2c3809216173bd02c237aab09b1a399efdba9
Andre <andremt2003@gmail.co>
2023-08-31 18:58:25 -0600

    San Valentin. TRBAJADO POR GABRIEL PAZ

--- LAB/app/src/main/java/com/example/lab3pm/FourthActivity.kt
+++ LAB/app/src/main/java/com/example/lab3pm/FourthActivity.kt
@@ -1,6 +1,8 @@
 package com.example.lab3pm
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class FourthActivity : AppCompatActivity() {
@@ -9,6 +11,25 @@ class FourthActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fourth)
 
-        // Inicializar los botones y configurar sus onClickListeners...
+        val btnSubMenu31: Button = findViewById(R.id.btnSubMenu31)
+        btnSubMenu31.setOnClickListener {
+            val intent = Intent(this@FourthActivity, IMG9ACT::class.java)
+            startActivity(intent)
+        }
+        val btnSubMenu32: Button = findViewById(R.id.btnSubMenu32)
+        btnSubMenu32.setOnClickListener {
+            val intent = Intent(this@FourthActivity, IMG10ACT::class.java)
+            startActivity(intent)
+        }
+        val btnSubMenu3: Button = findViewById(R.id.btnSubMenu33)
+        btnSubMenu3.setOnClickListener {
+            val intent = Intent(this@FourthActivity, IMG11ACT::class.java)
+            startActivity(intent)
+        }
+        val btnSubMenu34: Button = findViewById(R.id.btnSubMenu34)
+        btnSubMenu34.setOnClickListener {
+            val intent = Intent(this@FourthActivity, IMG12ACT::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ LAB/app/src/main/java/com/example/lab3pm/IMG10ACT.kt
@@ -0,0 +1,15 @@
+package com.example.lab3pm
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class IMG10ACT : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_img10)
+
+
+    }
+}
